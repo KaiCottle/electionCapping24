72fd9296a63de7f01f14a921571f3f1cf09371e7
AaronBonilla <a.bone1203@gmail.com>
2024-11-17 23:34:31 -0500

    reverted cors change

--- server.js
+++ server.js
@@ -26,17 +26,15 @@ const allowedOrigins = [
 
 // Configure CORS to allow requests from your React app
 app.use(cors({
-    origin: '*'
-    
-    // function (origin, callback) {
-    //     // Allow requests with no origin (like mobile apps or curl requests)
-    //     if (!origin) return callback(null, true);
-    //     if (allowedOrigins.indexOf(origin) === -1) {
-    //         const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-    //         return callback(new Error(msg), false);
-    //     }
-    //     return callback(null, true);
-    // }
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 
 app.use(express.json()); // Parse incoming JSON data
