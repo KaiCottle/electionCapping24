197127082d0df887bccaefd601f92c2b3ebb6a44
Ethan <ethan.ondreicka1@marist.edu>
2024-11-18 17:08:53 -0500

    added a 404

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AdminView from './AdminView'; // Admin view component
 import UserProfile from './UserProfile'; // User profile component
 import { AuthProvider, useAuth } from './AuthContext'; // Adjust the path as necessary
 import SSOCallback from './SSOCallback'; 
+import NotFound from './NotFound';
 import WebSocketComponent from './components/WebSocketClient';
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
             {/* Route for the SSO Callback */}
             <Route path="/sso-callback" element={<SSOCallback />} />
 
-            
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
     </AuthProvider>
---
+++ src/NotFound.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404 Error</h1>
+            <h1>Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
