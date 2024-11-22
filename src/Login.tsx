31f684ffaf28b584f280749d38522e94b2396208
Brendon Kupsch <98063893+BrendonKupsch1@users.noreply.github.com>
2024-11-22 01:49:32 -0500

    Testing sso button functionality

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Login.css';
 import backgroundImage from './assets/background.jpg';
-import logoImage from './assets/logo.png'
+import logoImage from './assets/logo.png';
 import Footer from './components/footer/footer';
 
 function Login() {
+  const handleSSOLogin = () => {
+    window.location.href = '/login'; // Redirect to the SSO login route
+  };
+
   return (
     <div className='m-login'>
       <img src={backgroundImage} className='bg-image' alt='Background' />
       <img src={logoImage} className='logo' alt='Marist Election Profile Logo' />
       <div className='login-box'>
         <h2 id='loginText'>Login:</h2>
-        <button className='sso-button'>Marist SSO</button>
+        <button className='sso-button' onClick={handleSSOLogin}>Marist SSO</button>
         <a href='/admin-login' className='admin-login'>Admin? Login here</a>
       </div>
     </div>
