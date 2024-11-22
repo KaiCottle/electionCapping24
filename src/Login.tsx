a8a5bc45b91aa77ba1297f2a6acabed69a136f00
Brendon Kupsch <98063893+BrendonKupsch1@users.noreply.github.com>
2024-11-22 14:34:02 -0500

    implemented login.tsx from ethan-dev

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Login.css';
 import backgroundImage from './assets/background.jpg';
-import logoImage from './assets/logo.png';
+import logoImage from './assets/logo.png'
 import Footer from './components/footer/footer';
 
-function Login() {
-  const handleSSOLogin = () => {
-    window.location.href = '/sso/login'; // Redirect to the SSO login route
-  };
+function handleSSOLogin() {
+  console.log('going to the /login')
+  window.location.href = 'https://facelect.capping.ecrl.marist.edu:3001/sso/login';
+}
 
+
+function Login() {
   return (
     <div className='m-login'>
       <img src={backgroundImage} className='bg-image' alt='Background' />
@@ -16,6 +18,7 @@ function Login() {
       <div className='login-box'>
         <h2 id='loginText'>Login:</h2>
         <button className='sso-button' onClick={handleSSOLogin}>Marist SSO</button>
+
         <a href='/admin-login' className='admin-login'>Admin? Login here</a>
       </div>
     </div>
