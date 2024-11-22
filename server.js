d447a189c7949273da82f4481c01a8560c77e9cb
Brendon Kupsch <98063893+BrendonKupsch1@users.noreply.github.com>
2024-11-22 01:43:36 -0500

    Readded idp_metadata because it vanished

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ passport.use(new SamlStrategy(
       path: '/login/callback',
       entryPoint: 'https://auth.it.marist.edu/idp/profile/SAML2/Redirect/SSO', // Updated entry point
       issuer: 'https://facelect.capping.ecrl.marist.edu',
-      cert: fs.readFileSync('./backend/2024_facelect.capping.ecrl.marist.edu.crt', 'utf-8'), // Replace with the path to your IdP certificate
+      cert: fs.readFileSync('./backend/idp_metadata.xml', 'utf-8'), // Replace with the path to your IdP certificate
     },
     function(profile, done) {
       findByEmail(profile.email, function(err, user) {
