8f4a009d63aaf318629277b33c9a7bdd9440708d
Brendon Kupsch <Brendon.Kupsch1@marist.edu>
2024-12-04 16:35:39 -0500

    Updated routing logic for sso login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';  // Login component
 import AdminLogin from './AdminLogin'; // Admin login component
 import AdminView from './AdminView'; // Admin view component
 import UserProfile from './UserProfile'; // User profile component 
 import { AuthProvider, useAuth } from './AuthContext'; // Adjust the path as necessary
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           <Route path='/User-Profile' element={<UserProfile />} />
 
           {/* Route for the SSO login page */}
-          <Route path="/sso/login" element={<div>Redirecting to SSO login...</div>} />
+          <Route path="/sso/login" element={<SsoLoginRedirect />} />
         </Routes>
       </Router>
     </AuthProvider>
@@ -44,5 +45,12 @@ const ProtectedAdminRoute = () => {
   );
 };
 
+const SsoLoginRedirect: React.FC = () => {
+  useEffect(() => {
+    window.location.href = 'https://facelect.capping.ecrl.marist.edu:3001/sso/login';
+  }, []);
+
+  return <div>Redirecting to SSO login...</div>;
+};
 
 export default App;
