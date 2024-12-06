fb5d0dd716d6ac92a52d40928df11c32bc4310be
Brendon Kupsch <98063893+BrendonKupsch1@users.noreply.github.com>
2024-12-06 14:49:07 -0500

    Revert "removed port from login"
    This reverts commit b01edaff02741cd4a2dc929431fb102b345d3c00.

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,7 +6,7 @@ import Footer from './components/footer/footer';
 
 function handleSSOLogin() {
   console.log('going to the /login')
-  window.location.href = 'https://facelect.capping.ecrl.marist.edu/sso/login';
+  window.location.href = 'https://facelect.capping.ecrl.marist.edu:3001/sso/login';
 }
 
 
